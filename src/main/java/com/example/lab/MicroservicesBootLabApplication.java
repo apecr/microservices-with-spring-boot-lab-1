4ef05637993405af6f339c87c3d91b1f0d1b6002
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-06-23 16:49:07 +0200

    first commit
    [FA-88]

---
+++ src/main/java/com/example/lab/MicroservicesBootLabApplication.java
@@ -0,0 +1,12 @@
+package com.example.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBootLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesBootLabApplication.class, args);
+	}
+}
